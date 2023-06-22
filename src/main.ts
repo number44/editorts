870f8fd916051ef05adada03cd792b03204ed290
number44 <daniel.lesiewicz.dev@gmail.com>
2023-06-22 14:00:03 +0200

    console.log fixed

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import "./style.scss";
-import EditorJS, { BlockToolConstructable } from "@editorjs/editorjs";
+import EditorJS, {
+  BlockToolConstructable,
+  LogLevels,
+} from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 import List from "@editorjs/list";
-import YoutubeEmbed from "editorjs-youtube-embed";
+// import { LogLevels } from "@editorjs/editorjs/types";
 
+import YoutubeEmbed from "editorjs-youtube-embed";
 new EditorJS({
   tools: {
     header: Header,
     List: List as BlockToolConstructable,
     youtubeEmbed: YoutubeEmbed,
   },
+  logLevel: LogLevels.ERROR,
 });
