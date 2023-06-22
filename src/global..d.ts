0a0b81b2ed0a21755478693286ff90b1b5ab488f
number44 <daniel.lesiewicz.dev@gmail.com>
2023-06-22 13:14:42 +0200

    youtube added

--- src/global..d.ts
+++ src/global..d.ts
@@ -2,3 +2,7 @@ declare module "@editorjs/list" {
   import List from "@editorjs/list";
   export = List;
 }
+declare module "editorjs-youtube-embed" {
+  import YoutubeEmbed from "editorjs-youtube-embed";
+  export = YoutubeEmbed;
+}
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import "./style.scss";
 import EditorJS, { BlockToolConstructable } from "@editorjs/editorjs";
 import Header from "@editorjs/header";
 import List from "@editorjs/list";
+import YoutubeEmbed from "editorjs-youtube-embed";
 
 new EditorJS({
   tools: {
     header: Header,
     List: List as BlockToolConstructable,
+    youtubeEmbed: YoutubeEmbed,
   },
 });
